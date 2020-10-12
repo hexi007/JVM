9c3fdf06e72d9bcb623aa901ce8d7ffce945460b
hexi007 <2777125829@qq.com>
2020-10-12 21:33:00 +0800

    Bytes数组处理工具

---
+++ src/classloader/ByteUtils.java
@@ -0,0 +1,12 @@
+package classloader;
+
+/**
+ * @author 27771
+ * @description Bytes数组处理工具
+ * @create 2020-10-12 21:30
+ **/
+public class ByteUtils {
+    public static int bytes2Int(byte[] classByte, int constantPoolCountIndex, int u2) {
+        return 0;
+    }
+}
